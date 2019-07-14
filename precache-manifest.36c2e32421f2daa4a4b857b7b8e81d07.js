ea6746f3041819ddba4e746cf4a6f3f489e0aa04
Vladimir Zorin <csum@mail.ru>
2019-07-14 23:04:20 +0400

    Booking test task deploy

---
+++ precache-manifest.36c2e32421f2daa4a4b857b7b8e81d07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4c96bcc144551e451e7e75f185103b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "731741ef18c199537a85",
+    "url": "/static/css/main.d2b78e0e.chunk.css"
+  },
+  {
+    "revision": "c73f713c2570bd79fa36",
+    "url": "/static/js/2.decbbdc6.chunk.js"
+  },
+  {
+    "revision": "731741ef18c199537a85",
+    "url": "/static/js/main.fcc8cdba.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
